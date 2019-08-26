b04b4d6e11fb048fb5c51ff9b853617c207d824c
Antonio-Zegarra-Becerra <54376590+Antonio-Zegarra-Becerra@users.noreply.github.com>
2019-08-25 23:23:40 -0500

    ejercicio 9 UPdated

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -5,8 +5,8 @@ int main() {
 
     int i = 1;
     while (i <= 10) {
-        if (   ) {
-            //imprimir numeros pares verticalmente
+        if (i % 2 ==0  ) {
+            cout<<i<<"\n"  //imprimir numeros pares verticalmente
         }
 
     }
